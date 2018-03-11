197412e547f06d4093d3e1c89a2e01520ad45a95
Joseph Dalrymple <joseph.dalrymple@bluelogicteam.com>
2018-03-11 06:05:16 -0500

    Added -n to echo command

--- src/commands/echo.js
+++ src/commands/echo.js
@@ -1,7 +1,32 @@
+import os from 'os';
+
+const man =
+`Usage: /echo [-n] text
+
+-n\tNo Newline\tOmits new line character after output
+
+Example:
+echo -n "Hello World"
+`;
+
 export default terminal => ({
-	handler: (...argv) => {
-		terminal.echo(argv.join(' '));
+	man,
+	handler: (...args) => {
+		const flags = {
+			eom: os.EOL
+		};
+
+		const msg = args.reduce((s, cV) => {
+			const ret = s ? [s] : [];
+			if (typeof cV === 'string') ret.push(cV);
+			else if (cV.name === 'n') {
+				flags.eom = '';
+				ret.push(cV.value);
+			}
+			return ret.join(' ');
+		}, '');
+
+		terminal.echo(`${msg}${flags.eom}`);
 		terminal.resolve('command.exit', 0);
-	},
-	man: 'Usage: /echo [text...]'
+	}
 });
